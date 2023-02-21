6116d24752f3d0f4d60880c9e37de26a24587df7
SuperBananeMan <damien.lolz.destructor@gmail.com>
2023-02-21 12:22:16 +0100

    corrigé

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css_index.css">
+    <link rel="stylesheet" href="src/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
@@ -18,13 +18,13 @@
 			<h1>Inscription</h1>
 			<form action="index.php" method="post">
 				<h3>Identifiant :</h3>
-					<input type="text" id="user" name="user"><br>
+					<input type="text" id="user" name="user" class="inp"><br>
 				<h3>Mot de passe :</h3>
-					<input type="password" id="passwrd" name="passwrd"><br>
+					<input type="password" id="passwrd" name="passwrd" class="inp"><br>
 				<h3>Vérification mot de passe :</h3>
-					<input type="password" id="verif-pass-word" name="verif-pass-word"><br>
+					<input type="password" id="verif-pass-word" name="verif-pass-word" class="inp"><br>
 				<h3>Email :</h3>
-					<input type="email" id="email" name="email"><br>
+					<input type="email" id="email" name="email" class="inp"><br>
 				<input type="submit" value="S'inscrire" id="enregistrer"><br>
 				<input type="reset" value="Effacer"><br>
 				
