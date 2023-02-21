8c9f72f2fb41cd755e13233f659e9baee0c072e6
Pingpetrus002 <86714770+Pingpetrus002@users.noreply.github.com>
2023-02-21 12:10:37 +0100

    test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <link type="stylesheet" href="src/styleheet.css">
+    <link rel="stylesheet" href="src/styleheet.css">
 
     
     <title>ISA Compta</title>
@@ -15,7 +15,96 @@
 </head>
 <body>
     
-    <p class="text-center h1 ">Hello World ! </p>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar_color">
+        <!-- Container wrapper -->
+        <div class="container">
+      <!-- Navbar brand -->
+            <a class="navbar-brand me-2" href="">
+            <img
+          src=""
+          height="16"
+          
+          loading="lazy"
+          style="margin-top: -1px;"
+            />
+            </a>
+        
+
+            <!-- Toggle button -->
+             <button
+                class="navbar-toggler"
+                type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#navbarButtonsExample"
+                    aria-controls="navbarButtonsExample"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                <i class="fas fa-bars"></i>
+                </button>
+      
+          <!-- Collapsible wrapper -->
+          <div class="collapse navbar-collapse" id="navbarButtonsExample">
+            <!-- Left links -->
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Dashboard</a>
+              </li>
+            </ul>
+            <!-- Left links -->
+        
+            <div class="d-flex align-items-center">
+              <button type="button" class="btn btn-link px-3 me-2">
+                <a href="login.php">Login</a>
+              </button>
+              <button type="button" class="btn btn-link me-3">
+                <a href="register.php">Sign Up</a> 
+              </button>
+
+            </div>
+          </div>
+          <!-- Collapsible wrapper -->
+        </div>
+        <!-- Container wrapper -->
+    </nav>
+    <!-- Navbar -->
+
+
+    <!-- Main Content -->
+
+
+    <div class="row">
+
+        <div class="col-md-6">
+
+            <p class="text-center"></p>
+
+
+
+
+        </div>
+
+        <div class="col-md-6">
+
+
+
+
+
+
+        </div>
+
+    
+    
+    
+    
+    
+    
+    </div>
+
+
+
 
 
 
