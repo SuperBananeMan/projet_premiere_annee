2772ee7e2cbbb8e3c953fffcea4064f0f7ed6917
SuperBananeMan <damien.lolz.destructor@gmail.com>
2023-03-07 10:25:17 +0100

    modif générale

--- login.php
+++ login.php
@@ -7,25 +7,25 @@
     <link rel="stylesheet" href="src/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+	<link rel="icon" type="image/x-icon" href="./src/assets/ico.png">
 
     
     <title>Connexion</title>
 
 </head>
 <body>
+	<div class="content_box">
     <div class="row d-flex justify-content-center">
-		<div class="col-md-3">
+		<div class="col-md-3 connexion_box">
 			<h1 class="d-flex justify-content-center">Connexion</h1>
 			
 		<form action="login.php" method="post">
 				<h3 class="var">Identifiant :</h3>
 					<input type="text" id="co_username" name="co_username" class="inp"><br>
-				<h3 class="var">Mot de passe :</h3>
-					<input type="password" id="co_passwrd" name="co_passwrd" class="inp"><br>
 				<h3 class="var">Email :</h3>
 					<input type="email" id="co_email" name="co_email" class="inp"><br>
-				<!--<h3>Code d'activation :</h3>
-					<input type="text" id="code-activation" name="code-activation"><br>-->
+				<h3 class="var">Mot de passe :</h3>
+					<input type="password" id="co_passwrd" name="co_passwrd" class="inp"><br>
 				<input type="submit" value="Se connecter"><br>
 				<input type="reset" value="Effacer"><br>
 				
@@ -57,6 +57,7 @@
 				?>
 			</form>
 		</div>
+	</div>
 	</div>
 
 
